cda801abacaa2ffc01476793d7c1991bb1dc7bc6
mpalma <mpalma.job@gmail.com>
2019-07-06 18:15:22 +0200

    menu

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import GlobalStyle from '../styles/global'
-import SEO from './SEO'
-import 'typeface-cantata-one'
-import 'typeface-open-sans'
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import GlobalStyle from "../styles/global"
+import SEO from "./SEO"
+import Menu from "./Menu"
+import "typeface-cantata-one"
+import "typeface-open-sans"
 
-const Layout = () => (
-  <>
-    <SEO />
-    <GlobalStyle />
-  </>
-)
+const Layout = ({ children }) => {
+    const { site } = useStaticQuery(
+        graphql`
+            query SiteQuery {
+                site {
+                    siteMetadata {
+                        menuLinks {
+                            link
+                            name
+                        }
+                    }
+                }
+            }
+        `
+    )
+
+    return (
+        <>
+            <SEO />
+            <GlobalStyle />
+            <Menu menu={site.siteMetadata.menuLinks} />
+            {children}
+        </>
+    )
+}
 
 export default Layout
