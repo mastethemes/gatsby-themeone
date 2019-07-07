103b932fb340cc722778866116398a5a1657bc01
Stilefano <stefanobucci@Mac-mini.station>
2019-07-07 19:31:59 +0200

    pagination for products

---
+++ gatsby-node.js
@@ -0,0 +1,15 @@
+const path = require(`path`)
+const productPages = require("./json/places.json")
+
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+    const template = path.resolve("./src/templates/product.jsx")
+
+    productPages.forEach(p => {
+        createPage({
+            path: p.url,
+            component: template,
+            context: p
+        })
+    })
+}
